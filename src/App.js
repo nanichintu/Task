4b4ddfddc74face3f1ecf680217ad77a29c176f6
sravankumar <sravankumar@5900gmail.com>
2023-06-23 13:45:06 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import  TextField  from "@mui/material/TextField";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?page=2")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredUsers = users.filter((user) =>
+    user.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <center>
+    <h1>User List</h1>
+    <TextField
+      type="text"
+      label="Search"
+      placeholder="Search by first name"
+      value={searchTerm}
+      onChange={handleSearch}
+    />
+    {filteredUsers.map((user) => (
+      <div key={user.id}>
+        <p>ID: {user.id}</p>
+        <img src={user.avatar} alt={user.first_name} />
+        <p>First Name: {user.first_name}</p>
+      </div>
+    ))}
+    </center>
+     
     </div>
   );
-}
+};
 
 export default App;
